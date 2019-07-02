0ada4ba4ba8572b2fff198a9e7f2ac00510c579c
Julien Schueller <schueller@phimeca.com>
2019-07-02 11:47:36 +0200

    Move feature test

--- test-pkg.sh
+++ test-pkg.sh
@@ -4,5 +4,7 @@ set -e
 
 for image in ubuntu debian centos fedora opensuse
 do
-  docker build ${image}
+  docker build ${image} -t ${image}
+  docker run ${image} sh -c "curl -fsSLO https://raw.githubusercontent.com/openturns/openturns/v1.13/python/test/t_features.py && python ./t_features.py"
 done
+
