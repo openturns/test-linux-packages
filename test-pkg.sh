0cc3812c1b46874c19d94df66423cf5953c65823
Julien Schueller <schueller@phimeca.com>
2020-10-27 11:16:06 +0100

    Update test-pkg.sh

--- test-pkg.sh
+++ test-pkg.sh
@@ -5,5 +5,5 @@ set -e
 for image in ubuntu debian centos fedora opensuse
 do
   docker build ${image} -t ${image}
-  docker run ${image} sh -c "curl -fsSLO https://raw.githubusercontent.com/openturns/openturns/v1.15/python/test/t_features.py && python3 ./t_features.py"
+  docker run ${image} sh -c "curl -fsSLO https://raw.githubusercontent.com/openturns/openturns/v1.16rc1/python/test/t_features.py && python3 ./t_features.py"
 done
