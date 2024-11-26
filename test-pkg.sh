3a2a6251314201f9708d992045a4eff0cf75df17
Julien Schueller <schueller@phimeca.com>
2024-11-26 15:23:26 +0100

    test: Bump OT version

--- test-pkg.sh
+++ test-pkg.sh
@@ -5,5 +5,5 @@ set -e
 for image in mageia centos fedora opensuse ubuntu debian
 do
   docker build ${image} -t ${image}
-  docker run ${image} sh -c "curl -fsSLO https://raw.githubusercontent.com/openturns/openturns/v1.24rc1/python/test/t_PlatformInfo_std.py && python3 ./t_PlatformInfo_std.py"
+  docker run ${image} sh -c "curl -fsSLO https://raw.githubusercontent.com/openturns/openturns/v1.24/python/test/t_PlatformInfo_std.py && python3 ./t_PlatformInfo_std.py"
 done
